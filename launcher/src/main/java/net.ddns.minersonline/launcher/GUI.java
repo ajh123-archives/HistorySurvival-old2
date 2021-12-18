bd0ed73eec66f529d598796b2bb856e87911ae9d
ajh123 <ajh123@users.noreply.github.com>
2021-12-18 13:33:47 +0000

    BEgin launcher GUI

---
+++ launcher/src/main/java/net.ddns.minersonline/launcher/GUI.java
@@ -0,0 +1,15 @@
+package net.ddns.minersonline.launcher;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
